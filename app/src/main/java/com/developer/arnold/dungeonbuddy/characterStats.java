96aadc053c3b3cab92739f510637d0f0fe691d65
arnoldGoncharenko1 <arnoldgon1994@gmail.com>
2016-06-08 21:21:30 -0400

    Started character stats (Point buy)

---
+++ app/src/main/java/com/developer/arnold/dungeonbuddy/characterStats.java
@@ -0,0 +1,13 @@
+package com.developer.arnold.dungeonbuddy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class characterStats extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_stats);
+    }
+}
