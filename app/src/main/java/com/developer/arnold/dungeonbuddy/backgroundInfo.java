95c956a350219e7a9b2ed01a8bfe1bb7b5be4d2d
arnoldGoncharenko1 <arnoldgon1994@gmail.com>
2016-09-18 01:21:55 -0400

    Re-added background list...somehow got deleted

---
+++ app/src/main/java/com/developer/arnold/dungeonbuddy/backgroundInfo.java
@@ -0,0 +1,13 @@
+package com.developer.arnold.dungeonbuddy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class backgroundInfo extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_background_info);
+    }
+}
