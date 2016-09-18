543f33cd1d9ed4d6b60193ebae341060439ff58e
arnoldGoncharenko1 <arnoldgon1994@gmail.com>
2016-09-18 00:38:28 -0400

    Started character stats (Point buy)

--- app/src/main/java/com/developer/arnold/dungeonbuddy/characterStats.java
+++ app/src/main/java/com/developer/arnold/dungeonbuddy/characterStats.java
@@ -31,7 +31,6 @@ public class characterStats extends Activity {
 
         int currentStrScore = Integer.parseInt(strScore.getText().toString());
         int currentPoints = Integer.parseInt(pointsLeft.getText().toString());
-
         boolean strPlusPressed = strPlus.isPressed();
         boolean strMinusPressed = strMinus.isPressed();
 
